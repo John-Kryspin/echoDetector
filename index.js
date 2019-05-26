7d6fbb5ee44b96c7e09d30811fee58c24f3d0779
John Kryspin <johnkryspin@Johns-MBP.home>
2019-05-25 21:53:39 -0400

    added sendPoopMessage on motion detection

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+require('dotenv').config()
 const Gpio = require('onoff').Gpio;
-const motionSensor = new Gpio(17, 'in','rising');
-motionSensor.watch((err, value) => {
+const motionSensor = new Gpio(17, 'in', 'rising');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+motionSensor.watch(async (err, value) => {
     if (err) {
         throw err;
     }
-        console.log(value)
-        console.log("Motion Detected")
-    
+    console.log(value)
+    console.log("Motion Detected")
+    await connectToDiscord();
+    await sendPoopMessage();
 })
 process.on('SIGINT', () => {
     motionSensor.unexport();
-  });
+});
+
+function connectToDiscord() {
+    return new Promise((resolve, reject) => {
+        client.login(process.env.DISCORD_KEY);
+        client.on('ready', () => {
+            console.log(`Logged in as ${client.user.tag}!`);
+            resolve()
+        });
+    })
+}
+async function sendPoopMessage() {
+    await client.channels.get(process.env.DISCORD_CHANNEL).send('It\'s time to scoopy the poopy!')
+    console.log("Message Sent!")
+}
